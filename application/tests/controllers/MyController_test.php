3126ad505a9edacd3050d06ed6df03d345a3317b
hallo i am kiki <arifahkinasih@gmail.com>
2017-10-21 11:19:04 +0700

    createuser

--- application/tests/controllers/MyController_test.php
+++ application/tests/controllers/MyController_test.php
@@ -83,18 +83,18 @@ class MyController_test extends TestCase
     }
     
     public function test_createuser(){
-        $totalrow=$this->objl->getTotalRow('halodea3','dea oct','ochadea12@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
+        $totalrow=$this->objl->getTotalRow('hulahup','hulala','hulahula@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
         $this->request('POST','MyController/aksi',
-                ['name'=>'dea oct',
+                ['name'=>'hulala',
                 'phone'=>'085674561210',
                 'address'=>'keputih gang 1',
-                'email'=>'ochadea12@gmail.com',
-                'username'=>'halodea3',
+                'email'=>'hulahula@gmail.com',
+                'username'=>'hulahup',
                 'password'=>'halodea1',
                 'confirmpw'=>'halodea1']);
-        $totalrowafter= $this->objl->getTotalRow('halodea','dea oct','ochadea12@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
+        $totalrowafter= $this->objl->getTotalRow('hulahup','hulala','hulahula@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
         $this->assertEquals($totalrowafter, $totalrow+1);
-        $this->objl->deleteRow('halodea3','dea oct','ochadea12@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
+        $this->objl->deleteRow('hulahup','hulala','hulahula@gmail.com', '085674561210', '271c68f0551dd9765b92f8bae4c1c257', 'keputih gang 1');
     }
     
     
